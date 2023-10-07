2225b5ebc367da959d92e708d62bd234c7c87cc6
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-07 10:52:10 +0530

    numeric half pyramid

---
+++ numeric_half_pyramid.cpp
@@ -0,0 +1,17 @@
+//Numeric half pyramid pattern
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ROWS = 8;
+    for (int row = 0; row < ROWS; row++)
+    {
+        for (int col = 0; col < row+1; col++)
+        {
+            cout << col+1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
