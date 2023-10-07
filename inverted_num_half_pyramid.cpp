047f6d01b3c59c47586d24b4254a5cc8f5a56af6
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-07 10:53:01 +0530

    inverted numeric half pyramid

---
+++ inverted_num_half_pyramid.cpp
@@ -0,0 +1,21 @@
+//Inverted numeric half pyramid pattern
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int ROWS = 8;
+    // for (int row = 0; row < ROWS; row++)
+    // {
+    //     for (int col = 0; col < row+1; col++)
+    //     {
+    //         cout << col+1;
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
+    int n=10;
+    if(cout <<n){
+        cout <<"Yash";
+    }
+}
